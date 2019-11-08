a90bc81e86ec96a5e23eac678fd824924e01225a
Daehan Hong <200307118@student.georgianc.on.ca>
2019-11-08 17:52:17 -0500

    integrating client and Server

---
+++ ConFusion Integrating the React Client and Server/conFusionServer/config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    'secretKey': '12345-67890-09876-54321',
+    'mongoUrl': 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientId: '569909607120236',
+        clientSecret: '1761455a0f5878a412dde475159b27ea'
+    }
+}
